58fb46e6ca8b09836e978d9aaac32494235c3100
HenryDatafy <hcarrillo@datafy.cl>
2023-09-07 16:43:16 -0300

    se agrga seccion sucursales y hazlo en casa

---
+++ src/app/eathome/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return <div>Hazlo en casa</div>;
+};
+
+export default page;
---
+++ src/app/sucursales/page.tsx
@@ -0,0 +1,15 @@
+import SucursalesView from "@/components/SucursalesView";
+import {
+  CloudArrowUpIcon,
+  LockClosedIcon,
+  ServerIcon,
+} from "@heroicons/react/20/solid";
+
+export default function Example() {
+  return (
+    <>
+      <SucursalesView />;
+      <SucursalesView />;
+    </>
+  );
+}
