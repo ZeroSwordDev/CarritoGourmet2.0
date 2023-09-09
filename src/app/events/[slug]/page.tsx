59f91f84c6587f08501b821e68862804c77ce2be
Juan Cruz <juanlimacaramico@gmail.com>
2023-09-09 19:33:36 -0300

    add events and hazloencasa .

---
+++ src/app/events/[slug]/page.tsx
@@ -0,0 +1,75 @@
+import { events } from "@/data";
+import Image from "next/image";
+import Link from "next/link";
+
+const EventPage = () => {
+  const individuals =
+    events.find((event) => event.slug === "individuals") || {};
+  console.log(individuals);
+  const corporatives =
+    events.find((event) => event.slug === "corporatives") || {};
+  // como traer el parametro de la url?
+  return (
+    <div className="w-full h-full flex text-red-500 justify-center items-center">
+      <div className="w-full h-full flex items-center justify-center">
+        {individuals && (
+          <div className="w-full border-r-2 border-b-2 border-red-500 w-full lg:w-2/3 p-4 flex flex-col justify-between group odd:bg-fuchsia-50">
+            {/* IMAGE CONTAINER */}
+            {individuals.img && (
+              <div className="self-center">
+                <Image
+                  src={individuals.img}
+                  alt=""
+                  height={500}
+                  width={500}
+                  className="object-contain"
+                />
+              </div>
+            )}
+            {/* TEXT CONTAINER */}
+            <div className="flex flex-col items-center justify-between font-bold gap-y-4">
+              <h1 className="text-2xl uppercase p-2">{individuals.title}</h1>
+              <h2 className="text-lg"> {individuals.desc} </h2>
+              <p className="text-sm">{individuals.fullDesc}</p>
+              <div>
+                {individuals.beneficts?.map((benefict) => (
+                  <p className="text-sm" key={benefict.id}>
+                    <span>{benefict.id}. </span>
+                    {benefict.desc}
+                  </p>
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+      {/* <div className="w-full">
+        {corporatives && (
+          <div className="w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-1/2 lg:w-1/3 p-4 flex flex-col justify-between group odd:bg-fuchsia-50">
+            {corporatives.img && (
+              <div className="relative h-[80%]">
+                <Image
+                  src={corporatives.img}
+                  alt=""
+                  fill
+                  className="object-contain"
+                />
+              </div>
+            )}
+            <div className="flex items-center justify-between font-bold">
+              <h1 className="text-2xl uppercase p-2">{corporatives.title}</h1>
+              <h2 className="group-hover:hidden text-xl">
+                ${corporatives.price}
+              </h2>
+              <button className="hidden group-hover:block uppercase bg-red-500 text-white p-2 rounded-md">
+                Add to Cart
+              </button>
+            </div>
+          </div>
+        )}
+      </div> */}
+    </div>
+  );
+};
+
+export default EventPage;
--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,21 +1,21 @@
-import { featuredProducts } from "@/data";
+import { hazloEnCasa, events, featuredProducts } from "@/data";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Featured = () => {
   return (
-    <div className="w-screen overflow-x-scroll text-gray-500">
+    <div className="w-full overflow-x-scroll text-gray-500">
       {/* WRAPPER */}
       <div className="w-max flex">
-        {/* SINGLE ITEM */}
-        {featuredProducts.map((item) => (
+        {events.map((item) => (
           <div
             key={item.id}
             className="w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]"
           >
             {/* IMAGE CONTAINER */}
             {item.img && (
-              <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-500">
+              <div className="relative flex-1 w-full">
                 <Image src={item.img} alt="" fill className="object-contain" />
               </div>
             )}
@@ -25,13 +25,35 @@ const Featured = () => {
                 {item.title}
               </h1>
               <p className="p-4 2xl:p-8">{item.desc}</p>
-              <span className="text-xl font-bold">${item.price}</span>
-              <button className="bg-red-500 text-white p-2 rounded-md">
-                Add to Cart
-              </button>
+              <Link href={`/events/${item.slug}`} className="bg-red-500 text-white p-2 rounded-md">
+                Ver eventos
+              </Link>
             </div>
           </div>
         ))}
+      
+          <div
+            key={hazloEnCasa.id}
+            className="w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]"
+          >
+            {/* IMAGE CONTAINER */}
+            {hazloEnCasa.img && (
+              <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-500">
+                <Image src={hazloEnCasa.img} alt="" fill className="object-contain" />
+              </div>
+            )}
+            {/* TEXT CONTAINER */}
+            <div className=" flex-1 flex flex-col items-center justify-center text-center gap-4">
+              <h1 className="text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl">
+                {hazloEnCasa.title}
+              </h1>
+              <p className="p-4 2xl:p-8">{hazloEnCasa.desc}</p>
+              <Link href={hazloEnCasa.slug} className="bg-red-500 text-white p-2 rounded-md">
+                Ver más
+              </Link>
+            </div>
+          </div>
+      
       </div>
     </div>
   );
