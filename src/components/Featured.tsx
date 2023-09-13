d6dce90f8f6417253527f447d678ff218b429ed5
ZeroSwordDev <Miguelworking1@gmail.com>
2023-09-12 21:38:58 -0300

    fixed img

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -39,7 +39,7 @@ const Featured = () => {
       
           <div
             key={hazloEnCasa.id}
-            className="w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]"
+            className="w-screen h-[60vh] flex flex-col items-center justify-around p-4 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]"
           >
             {/* IMAGE CONTAINER */}
             {hazloEnCasa.img && (
